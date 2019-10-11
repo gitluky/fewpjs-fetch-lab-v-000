c5ffb35e217a252e9535cd3bee51762e60701358
Jonathan Luk <jluk87@gmail.com>
2019-10-11 05:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function renderBooks(json) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks()
 })
+
+
+function getPosts() {
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(resp => resp.json())
+    .then(json => console.log(json));
+
+}
